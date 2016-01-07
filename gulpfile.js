53e40d4ca4b59548a46cdc791e2af3e28b9ce531
Morgan Owen <mog13@live.com>
2016-01-07 19:32:04 +0000

    created framework using karma/jasmine for unit testign and Gulp fior build process

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var Server = require('karma').Server;
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
---
+++ test/spec/test.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+
+  describe('Give it some context', function () {
+      it('should run here few assertions', function () {
+        expect(true).toBe(true);
+      });
+  });
+})();
