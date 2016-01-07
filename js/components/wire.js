9fe2d144e2e14ec5e591f548b3318c68d9e9a003
Morgan Owen <mog13@live.com>
2016-01-07 21:19:43 +0000

    Created Wire class and DigigtalIO. Created tests for functinality.

--- js/components/wire.js
+++ js/components/wire.js
@@ -1,4 +1,63 @@
+function Wire(startingValue, name, connections) {
+  this.name = name;
+  this.connections = connections|| [];
+  this.value = startingValue || 0;
+}
 
-var test = function() {
-return 4;
+//set a new value. If this changes the given value then update
+Wire.prototype.set = function set(newValue) {
+var oldValue = this.value;
+this.value = newValue;
+
+if(this.value !== oldValue) {
+this.update();
+}
+
+return this.value;
+};
+
+//add a function to list of functions to call on update
+Wire.prototype.addConnection = function addConnection(connection)
+{
+this.connections.push(connection);
+};
+
+//add many functions to list of functions to call on update
+Wire.prototype.addConnections = function addConnections(connections)
+{
+this.connections = this.connections.concat(connections);
+}
+//clear all functions in the list of functions to call on update
+Wire.prototype.clearConnections = function clearConnection(connection)
+{
+this.connections = [];
+};
+
+//call all the held connections
+Wire.prototype.update = function update()
+{
+	for(var i =0, len = this.connections.length; i <len; i++)
+	{
+		this.connections[i](this.value);
+	}
+};
+
+function DigIO(startingValue, threshold, name, connections) {
+  this.name = name;
+  this.connections = connections|| [];
+  this.value = startingValue || 0;
+  this.threshold = threshold;
+}
+DigIO.prototype = new Wire();
+
+DigIO.prototype.set = function set(newValue){
+ 
+var oldValue = this.value;
+this.value = newValue>this.threshold?1:0;
+
+if(this.value !== oldValue) {
+this.update();
+}
+
+return this.value;
 }
